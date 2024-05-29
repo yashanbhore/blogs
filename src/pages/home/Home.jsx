c91276677e8e759d200d44a881fde7a4034ed0a3
yashanbhore <anbhorey2003@gmail.com>
2024-05-29 15:37:05 +0530

    Home.js

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,9 +4,6 @@ import HeroSection from '../../components/heroSection/HeroSection'
 import BlogPostCard from '../../components/blogPostCard/BlogPostCard'
 
 function Home() {
-  useEffect(() => {
-    window.scrollTo(0, 0)
-}, [])
   return (
     <Layout>
         <HeroSection/>
